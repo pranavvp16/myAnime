df7fb21b8b33721a956cc6f2e177b40bea956e9b
Pranav Prajapati <pranavprajapati@Pranavs-MacBook-Pro.local>
2022-10-29 10:33:59 +0530

    flask

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from cgitb import html
-from flask import flask , render_template
+from flask import Flask , render_template
 import pickle as pkl 
-app = flask(__name__)
+app = Flask(__name__)
 
 anime_dict = pkl.load(open('pickle_data/anime_dict.pkl','rb'))
 similarity = pkl.load(open('pickle_data/similarity.pkl','rb'))
@@ -9,5 +9,5 @@ similarity = pkl.load(open('pickle_data/similarity.pkl','rb'))
 
 @app.route('/')
 def anime_name():
-    return "Hello World "
+    return "Hello World THis "
 
