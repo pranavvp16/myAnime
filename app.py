8d6ff7a9c17237bb4c46362daa99532a3e6696f8
Pranav Prajapati <pranavprajapati@Pranavs-MacBook-Pro.local>
2022-11-17 19:16:38 +0530

    Created recommendation page

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from cgitb import html
 from flask import Flask , render_template
 import pickle as pkl 
+import pandas as pd
 app = Flask(__name__)
+app.run(debug=True)
 
 anime_dict = pkl.load(open('pickle_data/anime_dict.pkl','rb'))
 similarity = pkl.load(open('pickle_data/similarity.pkl','rb'))
+anime_df = pd.DataFrame.from_dict(anime_dict)
 
 
 @app.route('/')
 def anime_name():
-    return "Hello World THis "
+    return render_template('index.html')
 
+@app.route('/recommend')
+def recommendation():
+    return " ON recommendation page"
