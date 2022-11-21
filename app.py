c4f43d0fc218d2b14d5f7ce7cfeab07a2a8e9e93
Pranav Prajapati <pranavprajapati586@gmail.com>
2022-11-21 23:02:45 +0530

    function to recommend anime

--- app.py
+++ app.py
@@ -6,8 +6,22 @@ app = Flask(__name__)
 
 anime_dict = pkl.load(open('pickle_data/anime_dict.pkl','rb'))
 similarity = pkl.load(open('pickle_data/similarity.pkl','rb'))
-anime_df = pd.DataFrame.from_dict(anime_dict)
-titles = anime_df["title"].values
+anime = pd.DataFrame.from_dict(anime_dict)
+titles = anime["title"].values
+
+
+def recommend(animes):
+  anime_index = anime[anime['title'] == animes].index[0]
+  distances = similarity[anime_index]
+  anime_list = sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
+
+  recommended_list = []
+  poster_image=[]
+
+  for i in anime_list:
+    recommended_list.append(anime.iloc[i[0]].title)
+    poster_image.append("anime_posters/"+str(i[0]) + '.jpg')
+  return recommended_list , poster_image
 
 
 @app.route('/')
@@ -23,6 +37,7 @@ def recommend():
 def recommendation():
     if request.method =="POST":
         title = request.form["title"]
-        return title
+        return recommend(title)
+
     else:
         return "Something went wrong"
